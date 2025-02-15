60239ff4dc61b6cd5410f579e07ab82b57675816
Yasin <yasinpeker43@gmail.com>
2025-02-15 20:21:18 +0300

    css elements are added.

---  music-player/app.js
+++  music-player/app.js
@@ -62,7 +62,7 @@ function nextMusic() {
 
 function pauseMusic() {
     container.classList.remove("playing");
-    play.classList="fa-solid fa-play";
+    play.querySelector("i").classList="fa-solid fa-play";
      audio.pause();
 
 
@@ -70,7 +70,7 @@ function pauseMusic() {
 
 function playMusic() {
     container.classList.add("playing");
-    play.classList="fa-solid fa-pause";
+    play.querySelector("i").classList="fa-solid fa-pause";
     audio.play();
 
 }
